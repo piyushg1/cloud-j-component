b81ea44f52a006b746aaa1ef128b16a911d46ad8
Piyush <piyushat25@gmail.com>
2023-05-24 19:14:09 +0530

    first

---
+++ README.md
@@ -0,0 +1,14 @@
+# React Todo App
+
+
+## Instructions
+
+Install dependencies. Make sure you already have [`nodejs`](https://nodejs.org/en/) & [`npm`](https://www.npmjs.com/) installed in your system.
+```bash
+$ npm install # or yarn
+```
+
+Run it
+```bash
+$ npm start # or yarn start
+```
---
+++ src/components/ui/TodoList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Info from './Info';
+import Header from './Header';
+import Footer from './Footer';
+import FilteredList from './FilteredList';
+import { applyFilter, search, FILTER_ACTIVE } from '../../services/filter';
+
+export default function TodoList(props) {
+    const { list, filter, mode, query } = props.data;
+    const { addNew, changeFilter, changeStatus, changeMode, setSearchQuery } = props.actions;
+    const activeItemCount = applyFilter(list, FILTER_ACTIVE).length;
+    const items = search(applyFilter(list, filter), query);
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="todolist">
+                    <Header {...{ addNew, mode, query, setSearchQuery }} />
+                    <FilteredList {...{ items, changeStatus }} />
+                    <Footer {...{ activeItemCount, filter, changeFilter, mode, changeMode }} />
+                    <Info {...{ mode }} />
+                </div>
+            </div>
+            <div className="last">
+                <br />
+                <br />
+                <br />
+                &copy; Cloud Computing J-Component 2023
+            </div>
+        </div>
+    );
+}
